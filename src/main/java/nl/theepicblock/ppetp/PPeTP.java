4445251c4114c32bf2ef54fef67f1b8b59dff1d3
kutaxoff <kutaxoff@icloud.com>
2025-05-01 12:04:54 +0300

    Add IndyPets compatability (ignore independent pets)

--- src/main/java/nl/theepicblock/ppetp/PPeTP.java
+++ src/main/java/nl/theepicblock/ppetp/PPeTP.java
@@ -3,6 +3,7 @@ package nl.theepicblock.ppetp;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,8 @@ public class PPeTP implements ModInitializer {
 	public static final String MOD_ID = "proper-pet-tp";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final boolean IS_INDYPETS_LOADED = FabricLoader.getInstance().isModLoaded("indypets");
 
 	public static final GameRules.Key<GameRules.BooleanRule> SHOULD_TP_CROSS_DIMENSIONAL =
 			GameRuleRegistry.register("petTeleportCrossDimension", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(false));
