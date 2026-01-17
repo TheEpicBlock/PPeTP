298ba395bd675ba435e108f9c93b0e8c2a780147
TheEpicBlock <git.teb@theepicblock.nl>
2026-01-18 00:26:00 +0100

    Add basic test

--- src/gametest/java/nl/theepicblock/gametest/PPeTPGameTest.java
+++ src/gametest/java/nl/theepicblock/gametest/PPeTPGameTest.java
@@ -1,55 +1,42 @@
 package nl.theepicblock.gametest;
 
-import com.mojang.serialization.Dynamic;
+import com.mojang.authlib.GameProfile;
 import net.fabricmc.fabric.api.entity.FakePlayer;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
-import net.minecraft.datafixer.Schemas;
-import net.minecraft.datafixer.TypeReferences;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtOps;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.storage.NbtReadView;
+import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.test.TestContext;
-import net.minecraft.util.ErrorReporter;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.GameMode;
 import nl.theepicblock.ppetp.PlayerDuck;
-import nl.theepicblock.ppetp.PlayerPetStorage;
-import nl.theepicblock.ppetp.test.Util;
 
-import static nl.theepicblock.ppetp.PPeTP.LOGGER;
+import java.util.UUID;
 
 public class PPeTPGameTest {
-    private static int LATEST_VERSION = 4661;
-
+    /// Teleports a fake player somewhat far, not far enough to unload chunks, to a place where a pet can't just tp to
     @GameTest
-    public void canReadOldData(TestContext context) throws Exception {
-        // This is from right before 1.21.11 was released, up until that point the old format might've still existed
-        canReadData(context, "/old_format.snbt", 4658);
-    }
+    public void basicTp(TestContext context) {
+        var world = context.getWorld();
+        var testCenter = context.getAbsolutePos(BlockPos.ORIGIN).toBottomCenterPos();
+        var player = FakePlayer.get(world, new GameProfile(UUID.randomUUID(), "Bob"));
+        player.getAbilities().allowFlying = true;
+        player.getAbilities().flying = true;
+        player.setPosition(testCenter);
 
-    @GameTest
-    public void canReadNewData(TestContext context) throws Exception {
-        canReadData(context, "/new_format.snbt", 4292);
-    }
+        var pet = new WolfEntity(EntityType.WOLF, world);
+        world.spawnEntity(pet);
+        pet.setTamedBy(player);
+        pet.setPosition(testCenter);
+        var uuidPet = pet.getUuid();
 
-    public void canReadData(TestContext context, String name, int version) throws Exception {
-        var oldPlayerData = Util.readNbtResource(name);
+        // Execute teleport
+        player.setPosition(testCenter.add(100, 500, 0));
 
-        // Update the nbt
-        var dyn = new Dynamic<>(NbtOps.INSTANCE, oldPlayerData);
-        var newNbt = context.getWorld().getServer().getDataFixer().update(TypeReferences.PLAYER, dyn, version, LATEST_VERSION).cast(NbtOps.INSTANCE);
-
-        // Load data
-        var reader = NbtReadView.create(new ErrorReporter.Logging(LOGGER), context.getWorld().getRegistryManager(), (NbtCompound)newNbt);
-        var player = FakePlayer.get(context.getWorld());
-        player.readData(reader);
-
-        // Do checks
-        var storage = ((PlayerDuck)player).PPeTP$getStorage();
-        context.assertEquals(1, storage.getNumberOfStoredPets(), "Should have decoded one pet");
-
-        // yay
-        context.complete();
+        // Should've tp'ed
+        context.waitAndRun(1, () -> {
+            context.assertTrue(world.getEntity(uuidPet) == null, "Pet should've teleported into PPeTP");
+            context.assertEquals(1, ((PlayerDuck)player).PPeTP$getStorage().getNumberOfStoredPets(), "Pet should've teleported into PPeTP");
+            context.complete();
+        });
     }
 }
---
+++ src/gametest/java/nl/theepicblock/gametest/PPeTPSerializationTest.java
@@ -0,0 +1,53 @@
+package nl.theepicblock.gametest;
+
+import com.mojang.authlib.GameProfile;
+import com.mojang.serialization.Dynamic;
+import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.datafixer.TypeReferences;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.storage.NbtReadView;
+import net.minecraft.test.TestContext;
+import net.minecraft.util.ErrorReporter;
+import nl.theepicblock.ppetp.PlayerDuck;
+import nl.theepicblock.ppetp.test.Util;
+
+import java.util.UUID;
+
+import static nl.theepicblock.ppetp.PPeTP.LOGGER;
+
+public class PPeTPSerializationTest {
+    private static int LATEST_VERSION = 4661;
+
+    @GameTest
+    public void canReadOldData(TestContext context) throws Exception {
+        // This is from right before 1.21.11 was released, up until that point the old format might've still existed
+        canReadData(context, "/old_format.snbt", 4658);
+    }
+
+    @GameTest
+    public void canReadNewData(TestContext context) throws Exception {
+        canReadData(context, "/new_format.snbt", 4292);
+    }
+
+    public void canReadData(TestContext context, String name, int version) throws Exception {
+        var oldPlayerData = Util.readNbtResource(name);
+
+        // Update the nbt
+        var dyn = new Dynamic<>(NbtOps.INSTANCE, oldPlayerData);
+        var newNbt = context.getWorld().getServer().getDataFixer().update(TypeReferences.PLAYER, dyn, version, LATEST_VERSION).cast(NbtOps.INSTANCE);
+
+        // Load data
+        var reader = NbtReadView.create(new ErrorReporter.Logging(LOGGER), context.getWorld().getRegistryManager(), (NbtCompound)newNbt);
+        var player = FakePlayer.get(context.getWorld(), new GameProfile(UUID.randomUUID(), "4555555555555555521111111111111111111111111111111111111111111fddddddddddddddddddddddddddddcc")); // Player name provided by Mia, my cat
+        player.readData(reader);
+
+        // Do checks
+        var storage = ((PlayerDuck)player).PPeTP$getStorage();
+        context.assertEquals(1, storage.getNumberOfStoredPets(), "Should have decoded one pet");
+
+        // yay
+        context.complete();
+    }
+}
